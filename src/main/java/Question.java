3e21840de3f841a82c6697ae8147c963e97158b2
Olena Borodych <olenaborodych@gmail.com>
2024-06-16 21:54:13 +0300

    initial commit

---
+++ src/main/java/Question.java
@@ -0,0 +1,40 @@
+import java.util.List;
+import java.util.Objects;
+
+public class Question {
+
+     private String question;
+     private List<String> answers;
+     private int correctAnswer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+//    public void setAnswers(List<String> answers) {}
+    public Question(String question, List<String> answers, int correctAnswer) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Question question1 = (Question) o;
+        return correctAnswer == question1.correctAnswer && Objects.equals(question, question1.question) && Objects.equals(answers, question1.answers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(question, answers, correctAnswer);
+    }
+}
